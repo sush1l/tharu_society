20360af6a0ba0771564212fe21c7354e31e607a3
Ninja PC 03 <tekam7755@gmail.com>
2023-12-21 17:30:54 +0545

    changes

--- resources/views/frontend/member.blade.php
+++ resources/views/frontend/member.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 @section('content')
     <div id="body">
-        <div class="about mt-4">
+        <div class="about mt-1">
             <div class="container">
                 <h1>{{ __('Members') }}</h1>
             </div>
@@ -9,29 +9,31 @@
     </div>
     <div class="container my-3">
         <div class="row">
-            <table class="table table-striped table-bordered mt-2">
-                <thead>
-                    <tr>
-                        <th scope="col">{{ __('S.N') }}</th>
-                        <th scope="col">{{ __('Name') }}</th>
-                        <th scope="col">{{ __('Designation') }}</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($members as $member)
-                        <tr>
-                            <th scope="row">{{ $loop->iteration }}</th>
-                            <td>
-                                {{ $member->title }}
-                            </td>
-                            <td>
-                                {{ $member->membershipCategory->title }}
-                            </td>
+            @if (!empty($members))
+            <div class="row">
+                <div class="col-md-12">
+                    <div id="team_card" class="card-container">
+                        <img class="rounded" src="{{$members->first()->photo ?? ''}}" height="150" weight="150"
+                             alt="{{$members->first()->name ?? ''}}">
 
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                        <p>{{$members->first()->title ?? ''}}</p>
+                        <p>{{$members->first()->membershipCategory->title ?? '' }}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                @foreach($members->skip(1) as $member)
+                <div class="col-md-3">
+                    <div class="card-container">
+                        <img class="rounded" src="{{$member->photo}}" height="150" width="150"
+                             alt="{{$member->name ?? ''}}">
+                            <p>{{$member->title ?? '' }}</p>
+                            <p>{{$member->membershipCategory->title ?? '' }}</p>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+            @endif
         </div>
     </div>
 @endsection

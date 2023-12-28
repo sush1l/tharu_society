0b86e56473f69f9b8938998c3bdde3b320c8998c
Ninja PC 03 <tekam7755@gmail.com>
2023-12-28 13:52:06 +0545

    change

--- resources/views/frontend/aboutus.blade.php
+++ resources/views/frontend/aboutus.blade.php
@@ -1,28 +1,23 @@
 @extends('layouts.master')
 @section('content')
-    <div id="body">
-        <div class="about mt-4">
+    <div data-aos="fade-up" id="body">
+        <div data-aos="fade-up" class="about mt-2">
             <div class="container">
-                <h1>{{ __('About Us') }}</h1>
+                <h1>{{ __('Introduction') }}</h1>
             </div>
         </div>
     </div>
     <div class="container my-3">
-        <div class="row">
-            <div class="intro">
-                <h2 class="text-center fw-bold">{{ __('Introduction') }}</h2>
-                <div class="container">
-                    <div class="wow fadeInUp" data-wow-delay="0.1s"
-                        style="visibility: visible; animation-delay: 0.1s; animation-name: fadeInUp;">
-                        <div class="col-sm-6 col-lg-12 mt-3">
-                            <h1 class="mb-3"></h1>
-                            <div class="card-01 h-100 mb-3">
-                                <p class="mx-3" style="text-align: justify;">{!! $officeDetail->description ?? '' !!}</p>
-                            </div>
-                        </div>
+        <section data-aos="fade-up" class="light-bg inner">
+                <div data-aos="fade-up" class="about-inner">
+                    <div class="content_image">
+                        <img class="img-responsive" src="{{asset('assets/frontend/images/team.jpg')}}" alt="About" align="">
+                        <p style="text-align: justify;">
+                            {!! $officeDetail->description ?? '' !!}
+                        </p>
                     </div>
                 </div>
-            </div>
-        </div>
+        </section>
     </div>
 @endsection
+

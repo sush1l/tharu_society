c5d962ff8d60d9f8cab8a361dc2dcc6bebce3e81
Ninja PC 03 <tekam7755@gmail.com>
2024-01-01 11:30:10 +0545

    change

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,11 @@
     <title>{{ config('app.name') }}</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}" />
+    <link
+    rel="shortcut icon"
+    href="{{asset('assets/frontend/images/logo.jpeg')}}"
+    type="image/x-icon"
+/>
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/css/style.css') }}">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
@@ -25,7 +29,7 @@
         </div>
     </button>
 
-    
+
         <div class="sub-header-card d-none d-sm-block">
             <div class="row">
                 <div class="col-md-6">
@@ -63,7 +67,7 @@
                 </div>
             </div>
         </div>
-   
+
     @include('frontend.partials.nav')
 
     @yield('content')

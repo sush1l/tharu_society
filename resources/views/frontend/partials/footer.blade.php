1186a869217021df23254eb51682b4a163885aac
Ninja PC 03 <tekam7755@gmail.com>
2024-01-09 16:56:26 +0545

    change

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -10,8 +10,9 @@
                                     <div class="footer_logo_container">
                                         <a href="#">
                                             <div class="footer_logo_text mt-4">
-                                                <p>THARU SOCIETY <br>
-                                                    SYDNEY AUSTRALIA </p>
+                                                <h5 class="fw-bold my-3">THARU <span
+                                                        >SOCIETY<br><span>SYDNEY</span>
+                                                        AUSTRALIA</span> </h5>
                                             </div>
                                         </a>
                                     </div>
@@ -20,7 +21,8 @@
                                     </div> --}}
                                     <div class="footer_social">
                                         <ul>
-                                            <li><a href="{{ $header->facebook_iframe }}"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                            <li><a href="{{ $header->facebook_iframe }}"><i class="fa fa-facebook"
+                                                        aria-hidden="true"></i></a>
                                             </li>
                                             {{-- <li><a href="#"><i class="fa fa-google-plus"
                                                         aria-hidden="true"></i></a>
@@ -38,8 +40,12 @@
                                     <div class="footer_title mt-4 text-center">{{ __('Contact') }}</div>
                                     <div class="footer_links_container text-white">
                                         <ul>
-                                            <li class="fw-bold">{{ __('Email') }}:<a href="mailto:{{ $header->office_email }}"> {{ $header->office_email }}</a></li>
-                                            <li class="fw-bold">{{ __('Phone') }}:<a href="tel:{{$header->office_phone}}"> {{ $header->office_phone }}</a></li>
+                                            <li class="fw-bold">{{ __('Email') }}:<a
+                                                    href="mailto:{{ $header->office_email }}">
+                                                    {{ $header->office_email }}</a></li>
+                                            <li class="fw-bold">{{ __('Phone') }}:<a
+                                                    href="tel:{{ $header->office_phone }}">
+                                                    {{ $header->office_phone }}</a></li>
                                             {{-- <li>{{ request()->language == 'en' ? $header->office_address_en : $header->office_address }} --}}
                                             </li>
                                         </ul>
@@ -53,7 +59,7 @@
                                         <ul>
                                             <li><a href="{{ route('welcome') }}">{{ __('Home') }}</a></li>
                                             <li><a href="{{ route('about-us') }}">{{ __('About Us') }}</a></li>
-                                            <li><a href="{{route('photo')}}">{{ __('Gallery') }}</a></li>
+                                            <li><a href="{{ route('photo') }}">{{ __('Gallery') }}</a></li>
                                             <li><a href="{{ route('contact') }}">{{ __('Contact') }}</a></li>
                                         </ul>
                                     </div>
@@ -68,7 +74,8 @@
                     <div class="copyright d-flex flex-lg-row flex-column align-items-center justify-content-start">
                         <div class="col-md-6 fw-bold">
                             Copyright ©
-                            {{ date('Y') }} All rights reserved by <a href="https://tharusociety.techworkcompany.com" class="text-white">TSSA</a>
+                            {{ date('Y') }} All rights reserved by <a
+                                href="https://tharusociety.techworkcompany.com" class="text-white">TSSA</a>
                         </div>
                         <div class="col-md-6 fw-bold">
                             <ul>
